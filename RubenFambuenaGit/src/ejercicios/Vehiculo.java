d2ef0e3786d7a7b5e731fa16ee54a340c2c3919b
windows10 <windows10@DESKTOP-B8E4IF5>
2021-05-25 09:57:22 +0200

    Subimos los archivos a GitHub

---
+++ RubenFambuenaGit/src/ejercicios/Vehiculo.java
@@ -0,0 +1,12 @@
+package ejercicios;
+
+public class Vehiculo {
+
+	private int numPuertas;
+	private int numRuedas;
+
+	public Vehiculo(int numPuertas, int numRuedas) {
+		this.numPuertas = numPuertas;
+		this.numRuedas = numRuedas;
+	}
+}
