afbac5429e02a5da0a91fd4ad3618dd3460afe8b
windows10 <windows10@DESKTOP-B8E4IF5>
2021-06-01 09:21:21 +0200

    Ultimo commit

--- RubenFambuenaGit/src/ejercicios/Moto.java
+++ RubenFambuenaGit/src/ejercicios/Moto.java
@@ -5,7 +5,7 @@ public class Moto extends Vehiculo {
 	private boolean llevaRadio;
 
 	public Moto(int numPuertas, int numRuedas) {
-		super(0, 2);
+		super();
 		llevaRadio = false;
 	}
 
--- RubenFambuenaGit/src/ejercicios/Vehiculo.java
+++ RubenFambuenaGit/src/ejercicios/Vehiculo.java
@@ -2,15 +2,10 @@ package ejercicios;
 
 public class Vehiculo {
 
-	private int numPuertas;
-	private int numRuedas;
-
-	public Vehiculo(int numPuertas, int numRuedas) {
-		this.numPuertas = numPuertas;
-		this.numRuedas = numRuedas;
-	}
-	
-	public void showInfo() {
-		System.out.println("Soy un vehiculo.");
+	int numRuedas = 4;
+	int numPuertas = 4;
+	boolean llevoRadio=true;
+	public Vehiculo() {
+		
 	}
 }
